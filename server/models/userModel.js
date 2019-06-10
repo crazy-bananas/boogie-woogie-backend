e11ed047b9086f4d9aa1b68cd0f5127737bd062e
Johannes Jarbratt and Shruti Jain <johannes.jarbratt+github+shrutiitengg@github.com>
2019-06-10 18:30:23 +0900

    more env

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 const { SongCollection } = require("./songs");
 require("dotenv").config();
 
-const COLLECTION = process.env.COLLECTION_NAME || "boogie";
-const USER_NAME = process.env.DB_USER_NAME;
+const COLLECTION_NAME_SONGS = process.env.COLLECTION_NAME_SONGS || "boogie";
+const DB_USER_NAME = process.env.DB_USER_NAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_URL = process.env.DB_URL || "boogie-u1a2t.mongodb.net";
 
 const connectDb = () => {
   return mongoose.connect(
-    `mongodb+srv://${USER_NAME}:${DB_PASSWORD}@boogie-u1a2t.mongodb.net/${COLLECTION}?retryWrites=true&w=majority`,
+    `mongodb+srv://${DB_USER_NAME}:${DB_PASSWORD}@${DB_URL}/${COLLECTION_NAME_SONGS}?retryWrites=true&w=majority`,
     {
       useCreateIndex: true,
       useNewUrlParser: true
