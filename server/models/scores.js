1993769c55e841dab91f644b1137fa44fc918a10
Shruti Jain <shrutiitengg@gmail.com>
2019-06-18 13:32:06 +0900

    changed schema for scores

--- server/models/scores.js
+++ server/models/scores.js
@@ -8,7 +8,8 @@ const scoreSchema = new mongoose.Schema({
   songId: { type: String },
   moveId: { type: String },
   user: { type: String },
-  score: { type: Number }
+  score: { type: Number },
+  pic: { type: String }
 });
 scoreSchema.index({ songId: 1, moveId: 1 }, { unique: true });
 
